52bb93b61c8643faa9be46dce524de809c90e37e
harika dameruppula <harikadameruppula2007@gmail.com>
2026-06-29 20:58:30 +0530

    Using Railway public URL

--- server/db/db.js
+++ server/db/db.js
@@ -2,18 +2,6 @@ require("dotenv").config();
 
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = db;
