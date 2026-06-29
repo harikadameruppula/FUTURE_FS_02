038169f7d3af9912d00ec1da623cf9a3fd97b5f0
harika dameruppula <harikadameruppula2007@gmail.com>
2026-06-29 20:50:58 +0530

    Fixed MySQL pool connection

--- server/db/db.js
+++ server/db/db.js
@@ -2,22 +2,17 @@ require("dotenv").config();
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {
     rejectUnauthorized: false
-  }
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Database connection failed:", err);
-  } else {
-    console.log("Database connected");
   }
 });
 
