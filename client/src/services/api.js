f0f91d3a89993ad81cd774243136a28a8ba9a53a
harika dameruppula <harikadameruppula2007@gmail.com>
2026-06-29 21:09:50 +0530

    Updated frontend backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+   baseURL: "https://mini-crm-backend-b9af.onrender.com/api"
 });
 
 export default API;
